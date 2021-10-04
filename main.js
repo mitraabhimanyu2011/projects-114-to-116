8b6d8ad7fb824e22315cce25035a22843599dd3a
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2021-10-03 18:01:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(700, 700);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function make_moustache()
+{
+    save("My Moustache Maker Image!.png");
+}
+
